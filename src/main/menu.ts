ddc65a2bb924ce68e2ab2f5f8a7c98e0872868b4
Matte <51192395+MatteZ02@users.noreply.github.com>
2025-03-15 13:50:12 +0200

    rebrand

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -57,14 +57,14 @@ export default class MenuBuilder {
       label: 'Electron',
       submenu: [
         {
-          label: 'About ElectronReact',
+          label: 'About Soundium',
           selector: 'orderFrontStandardAboutPanel:',
         },
         { type: 'separator' },
         { label: 'Services', submenu: [] },
         { type: 'separator' },
         {
-          label: 'Hide ElectronReact',
+          label: 'Hide Soundium',
           accelerator: 'Command+H',
           selector: 'hide:',
         },
